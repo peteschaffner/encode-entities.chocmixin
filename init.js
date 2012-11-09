bd8cfe9cfa74a9c64b010524ad23184494853f16
Pete Schaffner <pjschaffner@gmail.com>
2012-11-09 00:13:05 -0500

    Encode and decode actions working...
    ...still need to make ampersand encoding smarter

---
+++ init.js
@@ -0,0 +1,38 @@
+/*!
+ * Encode/decode HTML Entities mixin for Chocolat
+ * WTFPL 2 Licensed
+ */
+
+var ent = require('ent');
+
+// TODO: make encoding of ampersands intelligent
+// (where the "&" in &copy; wouldn't be encoded)
+Hooks.addMenuItem('Text/Convert/Encode HTML Entities', '', function () {
+  Recipe.run(function (r) {
+    var text = r.textInRange(r.selection),
+        encodedText = ent.encode(text).replace(
+          /&(lt|gt|apos|quot);/gi,
+          function (m, e) {
+            var entities = {
+              "gt"   : ">",
+              "lt"   : "<",
+              "quot" : "\"",
+              "apos" : "'"
+            };
+
+            return entities[e];
+          }
+        );
+
+    r.replaceTextInRange(r.selection, encodedText);
+  });
+});
+
+Hooks.addMenuItem('Text/Convert/Decode HTML Entities', '', function () {
+  Recipe.run(function (r) {
+    var text = r.textInRange(r.selection),
+        decodedText = ent.decode(text);
+
+    r.replaceTextInRange(r.selection, decodedText);
+  });
+});
